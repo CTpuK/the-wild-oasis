c9a87f24e6a5a0147187f81d8e396b54842089b1
Evgenyi <strik.jenya@gmail.com>
2023-11-03 22:11:15 +0300

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
   /* background-color: orangered; */
@@ -15,22 +15,30 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">The Wild Oasis</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
 
-          <div>
-            <Heading as="h2">Check in and out</Heading>
-            <Button onClick={() => alert('Check in')}>Check in</Button>
-            <Button onClick={() => alert('Check in')}>Check out</Button>
-          </div>
-        </Row>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check in")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Row type="vertical">
-          <Heading as="h3">Form</Heading>
-          <form>
-            <Input type="number" placeholder="Number of guests" />
-            <Input type="number" placeholder="Number of guests" />
-          </form>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
         </Row>
       </StyledApp>
     </>
