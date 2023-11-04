f3222606667fd95282774c5ddee29c01a9b29110
Evgenyi <strik.jenya@gmail.com>
2023-11-04 21:59:19 +0300

    3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,12 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.main`
-  /* background-color: orangered; */
-  padding: 20px;
-`;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis</Heading>
-
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button onClick={() => alert("Check in")}>Check in</Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert("Check in")}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of guests" />
-              <Input type="number" placeholder="Number of guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
