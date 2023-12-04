29ef8d7313b29341ff9c0a62dab9332e9c8515a7
Evgeny Strikunov <strik.jenya@gmail.com>
2023-12-04 12:14:52 +0300

    04.12

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://djtkteishwxsqiejenwc.supabase.co';
+export const supabaseUrl = 'https://djtkteishwxsqiejenwc.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqdGt0ZWlzaHd4c3FpZWplbndjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk4NTkzMDksImV4cCI6MjAxNTQzNTMwOX0.gcLZTIguSVQtgjTecQV1xX7jO7QSvlYcQqQ05pAwhDY';
 const supabase = createClient(supabaseUrl, supabaseKey);
